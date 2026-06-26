03c4e0aa69f6b7872c5c319a729b5b6bdfc15ac7
Mateus valentim <mateus.fvalentims@gmail.com>
2026-06-25 23:17:55 -0300

    Feat: Added Laravel Breeze and Next Breeze to the repo, now authorizaiton works

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,19 +3,18 @@
 use App\Http\Controllers\Api\V1\CategoryController;
 use App\Http\Controllers\Api\V1\NewsController;
 use App\Http\Controllers\Api\V1\TagController;
+use App\Http\Controllers\api\V1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('/hello', function () {
-    return ['message' => 'Hello World!'];
 });
 
-Route::prefix('v1')->group(function () {
-   Route::apiResource('categories', CategoryController::class);
-   Route::apiResource('tags', TagController::class);
-   Route::apiResource('news', NewsController::class);
+Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('tags', TagController::class);
+    Route::apiResource('news', NewsController::class);
+    Route::apiResource('users', UserController::class);
 });
+
