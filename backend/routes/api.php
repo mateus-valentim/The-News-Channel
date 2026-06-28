a3c7d48b6726c0671014e93da67b5759c2798169
Mateus valentim <mateus.fvalentims@gmail.com>
2026-06-28 15:02:14 -0300

    Feat: Added middleware to api requests

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\V1\CategoryController;
 use App\Http\Controllers\api\V1\DashboardController;
+use App\Http\Controllers\api\V1\ImageController;
 use App\Http\Controllers\Api\V1\NewsController;
 use App\Http\Controllers\Api\V1\TagController;
 use App\Http\Controllers\api\V1\UserController;
@@ -12,11 +13,12 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->group(function () {
+Route::middleware((['auth:sanctum']))->prefix('v1')->group(function () {
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('tags', TagController::class);
     Route::apiResource('news', NewsController::class);
     Route::apiResource('users', UserController::class);
     Route::get('/dashboard/stats', [DashboardController::class, 'getStats']);
+    route::post('/images/upload', [ImageController::class, 'StoreNewsImage']);
 });
 
